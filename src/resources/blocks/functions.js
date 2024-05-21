0682ac38f341ef3a5f0b5c41532ef9c504a9585c
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-05-21 16:34:37 +0200

    import blocks from turbobuilder dev

--- src/resources/blocks/functions.js
+++ src/resources/blocks/functions.js
@@ -147,5 +147,82 @@ function register() {
           }`;
         return `${code}\n`;
     })
+
+    // define func
+    registerBlock(`${categoryPrefix}define`, {
+        message0: 'define %1 %2',
+        args0: [
+            {
+                "type": "input_dummy"
+            },
+            {
+                "type": "input_statement",
+                "name": "BLOCKS"
+            }
+        ],
+        output: "Function",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const BLOCKS = javascriptGenerator.statementToCode(block, 'BLOCKS');
+        return [`(async (functionArg) => { ${BLOCKS} })`, javascriptGenerator.ORDER_NONE];
+    })
+
+    // execute
+    registerBlock(`${categoryPrefix}execute_a`, {
+        message0: 'execute %1 with %2',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "FUNC",
+                "check": "Function"
+            },
+            {
+                "type": "input_value",
+                "name": "ARG"
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const FUNC = javascriptGenerator.valueToCode(block, 'FUNC', javascriptGenerator.ORDER_ATOMIC);
+        const ARG = javascriptGenerator.valueToCode(block, 'ARG', javascriptGenerator.ORDER_ATOMIC);
+        const code = `${FUNC || "()=>{}"}(${ARG});`;
+        return `${code}\n`;
+    })
+    registerBlock(`${categoryPrefix}execute_b`, {
+        message0: 'execute %1 with %2',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "FUNC",
+                "check": "Function"
+            },
+            {
+                "type": "input_value",
+                "name": "ARG"
+            }
+        ],
+        output: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const FUNC = javascriptGenerator.valueToCode(block, 'FUNC', javascriptGenerator.ORDER_ATOMIC);
+        const ARG = javascriptGenerator.valueToCode(block, 'ARG', javascriptGenerator.ORDER_ATOMIC);
+        return [`(${FUNC || "()=>{}"}(${ARG}))`, javascriptGenerator.ORDER_NONE];
+    })
+
+    //arg
+    registerBlock(`${categoryPrefix}arg`, {
+        message0: 'function argument',
+        args0: [],
+        output: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        return [`functionArg`, javascriptGenerator.ORDER_NONE];
+    })
 }
 export default register;
