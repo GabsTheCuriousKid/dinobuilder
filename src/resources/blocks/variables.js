fd8dd6b8cf050d0e69cd395feb8c16f02199f7c5
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2024-09-18 22:56:35 +0200

    add more localstorage blocks

--- src/resources/blocks/variables.js
+++ src/resources/blocks/variables.js
@@ -71,6 +71,38 @@ function register() {
         const code = `localStorage.setItem(${NAME || '""'}, ${VAR || '""'})`;
         return `${code}\n`;
     })
+    // remove localStorage
+    registerBlock(`${categoryPrefix}removels`, {
+        message0: 'remove localstorage %1',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "NAME",
+                "checks": "String"
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const NAME = javascriptGenerator.valueToCode(block, 'NAME', javascriptGenerator.ORDER_ATOMIC);
+        const code = `localStorage.removeItem(${NAME || '""'})`;
+        return `${code}\n`;
+    })
+
+    // clear localStorage
+    registerBlock(`${categoryPrefix}clearls`, {
+        message0: 'clear localstorage',
+        args0: [],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const code = `localStorage.clear()`;
+        return `${code}\n`;
+    })
 
     // get localStorage
     registerBlock(`${categoryPrefix}getls`, {
@@ -88,6 +120,24 @@ function register() {
     }, (block) => {
         const NAME = javascriptGenerator.valueToCode(block, 'NAME', javascriptGenerator.ORDER_ATOMIC);
         return [`localStorage.getItem(${NAME || '""'})`, javascriptGenerator.ORDER_ATOMIC];
+    })
+
+    // does localStorage exist
+    registerBlock(`${categoryPrefix}lsexists`, {
+        message0: 'does localstorage %1 exist?',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "NAME",
+                "checks": "String"
+            }
+        ],
+        output: "boolean",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const NAME = javascriptGenerator.valueToCode(block, 'NAME', javascriptGenerator.ORDER_ATOMIC);
+        return [`localStorage.getItem(${NAME || '""'} !== null)`, javascriptGenerator.ORDER_ATOMIC];
     })
 }
 
