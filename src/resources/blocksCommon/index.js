e71122c0e5287bb4c11d07acc19a60d50fc35787
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-08-10 15:19:12 +0200

    test

--- src/resources/blocksCommon/index.js
+++ src/resources/blocksCommon/index.js
@@ -1,7 +1,11 @@
+import Blockly from 'blockly/core';
+import 'blockly/core/colours';
+
 import registerMath from './math'
 import registerText from './text'
 
 export default function() {
+    console.log(Blockly.Colours)
     registerMath()
     registerText()
 }
