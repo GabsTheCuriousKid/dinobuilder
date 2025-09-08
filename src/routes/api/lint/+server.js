edd5d68113dd3cfcd30bd489ac3a6d5b32ca4036
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-09-08 20:04:50 +0200

    why is eslint so hard to add :(

--- src/routes/api/lint/+server.js
+++ src/routes/api/lint/+server.js
@@ -5,6 +5,7 @@ const { ESLint } = require('eslint');
 
 async function checkForErrors(code) {
     const eslint = new ESLint({ useEslintrc: false, baseConfig: { extends: "eslint:recommended" } });
+    window.ESLINT = eslint
     const results = await eslint.lintText(code);
     return results[0].messages;
 }
